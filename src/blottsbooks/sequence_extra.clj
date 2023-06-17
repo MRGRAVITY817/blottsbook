51b3c73bfe11eae250c9968903eddbc045e9490a
Hoon Wee <mrgravity817@gmail.com>
2023-06-18 02:07:03 +0900

    extra stuffs with seq

--- src/blottsbooks/sequence_extra.clj
+++ src/blottsbooks/sequence_extra.clj
@@ -1,7 +1,8 @@
-(ns blottsbooks.read-files
+(ns blottsbooks.sequence-extra
   (:require
    [clojure.java.io :as io]))
 
+;; Reading files as sequence
 (defn listed-author? [author]
   (with-open [r (io/reader "authors.txt")]
     ; line-seq turn contents into sequence
@@ -10,4 +11,12 @@
 (listed-author? "JK Rowling") ;=> true
 (listed-author? "Jung Rae Cho") ;=> true
 (listed-author? "Hoon Wee") ;=> nil
+
+;; Regular expressions
+(def re #"Pride and Prejudice.*")
+(def title "Pride and Prejudice and Zombies")
+(if (re-matches re title)
+  (println "We have a classic!"))
+(re-seq #"\w+" title) ;=> ("Pride" "and" "Prejudice" "and" "Zombies")
+
 
