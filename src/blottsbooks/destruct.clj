6f6922d31b974f68675d131e37afafde91d2c065
Hoon Wee <mrgravity817@gmail.com>
2023-06-18 12:55:40 +0900

    destructuring map

--- src/blottsbooks/destruct.clj
+++ src/blottsbooks/destruct.clj
@@ -22,3 +22,34 @@
 (let [[[painter] [_ poet]] pairs]
   (println "The painter is" painter)
   (println "The poet is" poet))
+
+;; Not only vectors, but we can also destruct any seq-like data structures.
+(let [[c1 c2 c3 c4] "Harry"]
+  (println c1)
+  (println c2)
+  (println c3)
+  (println c4))
+
+;; We can also destruct function args
+(defn artist-description [[novelist poet]]
+  (str "The novelist is " novelist " and the poet is " poet))
+
+(artist-description [:austen :dickinson])
+
+;; We can destruct maps
+(def artist-map {:painter :monet :novelist :austen})
+(let [{painter :painter writer :novelist} artist-map]
+  (println "The painter is" painter)
+  (println "The writer is" writer))
+
+;; Destructuring map is reversible
+(let [{writer :novelist painter :painter} artist-map]
+  (println "The writer is" writer)
+  (println "The painter is" painter))
+
+;; Destructuring nested map
+(def austen {:name "Jane Austen"
+             :parents {:father "George" :mother "Cassandra"}
+             :dates {:born 1775 :died  1817}})
+(let [{{dad :father mom :mother} :parents} austen]
+  (println "Hello" mom "and" dad "!"))
