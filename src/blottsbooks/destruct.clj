4dc6415f1854e27f2a07e053317d00cca5ea083f
Hoon Wee <mrgravity817@gmail.com>
2023-06-18 11:59:21 +0900

    ignore items and destruct nested seq

--- src/blottsbooks/destruct.clj
+++ src/blottsbooks/destruct.clj
@@ -1,7 +1,24 @@
 (ns blottsbooks.destruct)
 
-(def artists [:monet :austen])
+(def artists [:monet :austen :charlieputh :jayz])
+
 ;; Destructure with vector - first/second item
 (let [[painter novelist] artists]
   (println "The painter is:" painter)
   (println "and the novelist is:" novelist))
+
+;; Ignore some of the leading elements 
+(let [[_ _ singer rapper] artists]
+  (println "The singer is:" singer)
+  (println "The rapper is:" rapper))
+
+(def pairs [[:monet :austen] [:beethoven :dickinson]])
+
+;; Destructure nested sequence
+(let [[[painter] [composer]] pairs]
+  (println "The painter is" painter)
+  (println "The composer is" composer))
+
+(let [[[painter] [_ poet]] pairs]
+  (println "The painter is" painter)
+  (println "The poet is" poet))
