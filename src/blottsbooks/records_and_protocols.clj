9418a53030c5e1fa472fa207f5d6af3a6c277801
Hoon Wee <mrgravity817@gmail.com>
2023-06-18 15:23:52 +0900

    protocols are like traits

--- src/blottsbooks/records_and_protocols.clj
+++ src/blottsbooks/records_and_protocols.clj
@@ -35,6 +35,34 @@
     (println "Fictional")
     (println "Computer")))
 
+;; Protocols are set _how to behave_ rules
+;; It's like _traits_ in Rust
+(defprotocol Person
+  (full-name [this])
+  (greeting [this msg])
+  (description [this]))
+
+(defrecord FakeCharacter [name appears-in author]
+  Person
+  (full-name [this] (:name this))
+  (greeting [this msg] (str msg " " (:name this)))
+  (description [this]
+    (str (:name this) " is a character in " (:appears-in this))))
+
+(defrecord Employee [first-name last-name department]
+  Person
+  (full-name [this] (str first-name " " last-name))
+  (greeting [this msg] (str msg " " (:first-name this)))
+  (description [this]
+    (str (:first-name this) " works in " (:department this))))
+
+(def sofia (->Employee "Sofia" "Diego" "Finance"))
+(def sam (->FakeCharacter "Sam Waller" "The Pickwick Papers" "Dickens"))
+
+(full-name sofia) ;=> "Sofia Diego"
+(description sam) ;=> "Sam Waller is a character in The Pickwick Papers"
+
+
 
 
 
