e0410b62bdceaa25025f4f999673fb5eb08a566b
Hoon Wee <mrgravity817@gmail.com>
2023-06-21 00:21:00 +0900

    use quote for easy macro construction

--- src/blottsbooks/macros.clj
+++ src/blottsbooks/macros.clj
@@ -29,3 +29,34 @@
         :else neg))
 
 (arithmetic-if rating :loved-it :meh :hated-it)
+
+;; Using quotes
+;; Quote gives us full-path for pos? zero? etc.
+(def n 100)
+(def pos "It's positive")
+(def zero "It's zero!")
+(def neg "It's negative!")
+
+`(cond
+   (pos? ~n) ~pos
+   (zero? ~n) ~zero
+   :else ~neg)
+
+;;=> (clojure.core/cond
+;;=>  (clojure.core/pos? 100)
+;;=>  "It's positive"
+;;=>  (clojure.core/zero? 100)
+;;=>  "It's zero!"
+;;=>  :else
+;;=>  "It's negative!")
+
+(defmacro arithmetic-if-quoted [n pos zero neg]
+  `(cond
+     (pos? ~n) ~pos
+     (zero? ~n) ~zero
+     :else ~neg))
+
+;; Referring values within quotes
+;; @ will expand body, peeling off the parens.
+(defmacro our-defn [name args & body]
+  `(def ~name (fn ~args ~@body)))
